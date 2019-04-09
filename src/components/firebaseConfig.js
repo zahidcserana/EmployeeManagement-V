dd01687f9b88a7e8a41bb248449179c50bdc28d4
zahidcserana <zislam@leapinglogic.com>
2019-04-09 10:08:51 +0600

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCCJDp9IqngVdFHmYiD4rd_91nZbVRJUtg",
+    authDomain: "employeemanagement-51c8e.firebaseapp.com",
+    databaseURL: "https://employeemanagement-51c8e.firebaseio.com",
+    projectId: "employeemanagement-51c8e",
+    storageBucket: "employeemanagement-51c8e.appspot.com",
+    messagingSenderId: "939301476782"
+}
